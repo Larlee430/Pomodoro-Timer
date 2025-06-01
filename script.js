42592392d063ff07c645a8cd3b8f715813e3fdef
Junyun <211919026+Larlee430@users.noreply.github.com>
2025-05-31 23:31:44 -0400

    Set countdown and break button when count is done

---
+++ script.js
@@ -0,0 +1,22 @@
+const startingMinute = .1;
+let time = startingMinute * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+const interval = setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+
+    if (time < 0) {
+        clearInterval(interval);
+        countdownEl.innerHTML = `<button id="startBreakBtn">Start Break</button>`;
+        document.getElementById('startBreakBtn').onclick = function() {
+            window.location.href = "coffeebreak.html";
+        };
+    }
+}
